6a9765935942b0c5d42187fb28f4256eeedb9cd6
Whoistolu <digittolu25@gmail.com>
2022-05-30 11:49:56 +0100

    Addmore data

--- src/data.js
+++ src/data.js
@@ -1,37 +1,46 @@
-export default [
+const data = [
   {
     id: 1,
     name: 'Bertie Yates',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      '/src/images/1.jpg',
   },
   {
     id: 2,
     name: 'Hester Hogan',
     age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    '/src/images/2.jpg',
   },
   {
     id: 3,
     name: 'Larry Little',
     age: 36,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    '/src/images/3.jpg',
   },
   {
     id: 4,
     name: 'Sean Walsh',
     age: 34,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    '/src/images/4.jpg',
   },
   {
     id: 5,
     name: 'Lola Gardner',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    '/src/images/5.jpg',
+  },
+  {
+    id: 6,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+    '/src/images/6.jpg',
   },
 ];
+
+export default data;
